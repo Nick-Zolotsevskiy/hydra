fa575412e2d945b6a2dfe9b25dc61f5708b5fc97
Nick-Zolotsevskiy <zoloce2000@yandex.ru>
2021-12-27 14:28:37 +0300

    Create API views

--- hydra_shop/urls.py
+++ hydra_shop/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path('categories/', views.categories_list),
     path('categories/<int:category_id>/', views.products_list),
     path('products/<int:product_id>/', views.product_info),
+
+    path('api/categories/', views.CategoryListCreateAPIView.as_view()),
+    path('api/categories/<int:pk>/', views.CategoryRetrieveUpdateDestroyAPIView.as_view()),
+
+    path('api/products/', views.ProductListCreateAPIView.as_view()),
+    path('api/products/<int:pk>/', views.ProductRetrieveUpdateDestroyAPIView.as_view()),
+
+    path('api/products/<int:product_id>/images/', views.ProductImageListCreateAPIView.as_view()),
+    path('api/products/images/<int:pk>/', views.ProductImageRetrieveUpdateDestroyAPIView.as_view()),
 ]
--- hydra_shop/views.py
+++ hydra_shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from hydra_shop.models import Category, Product, ProductImage
+from hydra_shop.serializers import CategorySerializer, ProductSerializer, ProductImageSerializer
 
 
 def index(request):
@@ -22,3 +24,37 @@ def product_info(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     images = ProductImage.objects.filter(product=product)
     return render(request, 'product_info.html', {'product': product, 'images': images})
+
+
+# REST API
+
+class CategoryListCreateAPIView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class ProductImageListCreateAPIView(ListCreateAPIView):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
+    lookup_fields = 'product_id'
+    lookup_url_kwarg = 'product_id'
+
+
+class ProductImageRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
